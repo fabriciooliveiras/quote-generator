3e2dee87e2dee449e14e9046417129852f273bf1
Fabricio Oliveira <fabricio.oliveira.s@hotmail.com>
2024-04-18 00:08:18 -0300

    Loader Added

--- script.js
+++ script.js
@@ -25,10 +25,33 @@ async function getQuotes(){
 }
 
 function newQuote(){
+    
+    loader("show");
+
+    //HIDE OLD INFO
+    document.getElementById("quote-text").style.opacity = "0";
+    document.getElementById("author").style.opacity = "0";
+
     //GET RANDOM QUOTE
     currentQuote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+
+    loader("hide");
+
+    //SHOW NEW INFO
+    document.getElementById("quote-text").style.opacity = "1";
+    document.getElementById("author").style.opacity = "1";
+    
     //UPDATES THE UI
     document.getElementById("quote").innerText = currentQuote.text;
     let author = (currentQuote.author != null) ? currentQuote.author : "Unknown author";
     document.getElementById("author").innerText = author;
+
+}
+
+function loader(status){
+    if(status == "show"){
+        document.getElementById("loader-container").classList.remove("hidden");
+    }else{
+        document.getElementById("loader-container").classList.add("hidden");
+    }
 }
